1838f7233fdba3aa2f04cd6f94b7925fc75d6b4c
Mubtasim Fuad <m.fuad.rafi@gmail.com>
2021-08-14 10:58:00 +0600

    Add Bangla text for download count

--- simple-download-monitor/includes/templates/fancy1/sdm-fancy-1.php
+++ simple-download-monitor/includes/templates/fancy1/sdm-fancy-1.php
@@ -137,8 +137,8 @@ function sdm_generate_fancy1_display_output( $args ) {
     }
 
     $db_count		 = sdm_get_download_count_for_post( $id );
-    $string			 = ($db_count == '1') ? __( 'Download', 'simple-download-monitor' ) : __( 'Downloads', 'simple-download-monitor' );
-    $download_count_string	 = '<span class="sdm_item_count_number">' . $db_count . '</span><span class="sdm_item_count_string"> ' . $string . '</span>';
+    $string			 = ($db_count == '1') ? __( 'মোট ডাউনলোড সংখ্যা: ', 'simple-download-monitor' ) : __( 'মোট ডাউনলোড সংখ্যা: ', 'simple-download-monitor' );
+    $download_count_string	 = '<span class="sdm_item_count_string"> ' . $string . '</span><span class="sdm_item_count_number">' . $db_count . '</span>';
 
     $output = '';
 
